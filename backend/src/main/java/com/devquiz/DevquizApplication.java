8f2fa852add74c306393c9b584bd46923ff8241c
beomsic <hhqjatjr@gmail.com>
2026-01-03 14:16:32 +0900

    chore: backend init (java + springboot)

---
+++ backend/src/main/java/com/devquiz/DevquizApplication.java
@@ -0,0 +1,13 @@
+package com.devquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevquizApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devquiz/DevquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
